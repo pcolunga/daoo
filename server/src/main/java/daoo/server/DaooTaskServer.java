e48726e02d4a3c5aac38618ee61acf717dc28e71
Pedro Colunga <pedro@tekgenesis.com>
2013-05-24 15:42:03 -0300

    Daoo Task Server

--- server/src/main/java/daoo/server/DaooTaskServer.java
+++ server/src/main/java/daoo/server/DaooTaskServer.java
@@ -9,21 +9,7 @@ import java.net.Socket;
 import static daoo.server.Task.*;
 
 public class DaooTaskServer implements TaskServer {
-
     @Override public void start(@NotNull TaskExecutor executor, int port) {
-
-        try {
-            final ServerSocket server = new ServerSocket(port);
-            System.out.println("Listening on port: " + port);
-
-            //noinspection InfiniteLoopStatement
-            for (;;) {
-                executor.execute(sleepy(server.accept(), 5000));
-//                executor.execute(infinite(server.accept()));
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        throw new IllegalStateException("To be implemented!");
     }
 }
